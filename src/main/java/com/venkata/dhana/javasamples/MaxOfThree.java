ba24d9952b2babd249421e5d8e85c8202718a72e
suresh <suresh@sureshs-MacBook-Pro.local>
2017-05-04 23:34:59 +0100

    Cleanup logic for maximum of 3 numbers

--- src/main/java/com/venkata/dhana/javasamples/MaxOfThree.java
+++ src/main/java/com/venkata/dhana/javasamples/MaxOfThree.java
@@ -5,21 +5,37 @@ import java.util.Scanner;
 
 public class MaxOfThree {
     public static void main(String[] args) {
+
         System.out.println("enter 3 num");
+
         Scanner sc = new Scanner(System.in);
 
         int i = sc.nextInt();
         int j = sc.nextInt();
         int k = sc.nextInt();
 
-        if (i >= j && i >= k) {
-            System.out.println("Result is " + i);
-        } else if (j >= i && j >= k) {
-            System.out.println("Result is " + j);
-        } else if (k >= i && k >= j) {
-            System.out.println("Result is " + k);
+//        if (i >= j && i >= k) {
+//            System.out.println("Result is " + i);
+//        } else if (j >= i && j >= k) {
+//            System.out.println("Result is " + j);
+//        } else if (k >= i && k >= j) {
+//            System.out.println("Result is " + k);
+//        } else {
+//            System.out.println("Invalid");
+//        }
+
+        if (i >= j) {
+            if (i >= k) {
+                System.out.println("Max of given numbers is = " + i);
+            } else {
+                System.out.println("Max of given numbers is = " + k);
+            }
         } else {
-            System.out.println("Invalid");
+            if (j >= k) {
+                System.out.println("Max of given numbers is = " + j);
+            } else {
+                System.out.println("Max of given numbers is = " + k);
+            }
         }
 
     }
